3773b362a40829c9d06fe32052e1c634b24d3476
bryanares <bryanares12@gmail.com>
2022-04-05 14:23:21 +0300

    create recyclerview for the list of astro

---
+++ app/src/main/java/com/brian/astro/AstroApi.kt
@@ -0,0 +1,4 @@
+package com.brian.astro
+
+interface AstroApi {
+}
--- app/src/main/java/com/brian/astro/HomeFragment.kt
+++ app/src/main/java/com/brian/astro/HomeFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.brian.astro.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
+    private var _binding: FragmentHomeBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +23,9 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
 }
