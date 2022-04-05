dbb61a256529121a0b00a8ceee66c4060ea5454e
bryanares <bryanares12@gmail.com>
2022-04-05 14:35:46 +0300

    create Astro data class and retrofit instance

--- app/src/main/java/com/brian/astro/AstroApi.kt
+++ app/src/main/java/com/brian/astro/AstroApi.kt
@@ -1,4 +1,10 @@
 package com.brian.astro
 
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface AstroApi {
+
+    @GET("/planetary/apod?api_key=DEMO_KEY&count=10")
+    suspend fun getAstros(): Response<List<Astro>>
 }
---
+++ app/src/main/java/com/brian/astro/RetrofitInstance.kt
@@ -0,0 +1,4 @@
+package com.brian.astro
+
+object RetrofitInstance {
+}
