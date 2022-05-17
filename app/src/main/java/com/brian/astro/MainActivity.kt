6d64b7496942b55288d463b44af6f8acbbf0e10f
bryanares <bryanares12@gmail.com>
2022-05-17 16:32:20 +0300

    set up navController

--- app/src/main/java/com/brian/astro/MainActivity.kt
+++ app/src/main/java/com/brian/astro/MainActivity.kt
@@ -5,9 +5,15 @@ import android.os.Bundle
 import android.util.Log
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.brian.astro.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import java.io.IOException
 import java.net.HttpRetryException
 
@@ -19,6 +25,16 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        val bottomNavigationView= findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        //AppBar
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment, R.id.bookMarkFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+
+        bottomNavigationView.setupWithNavController(navController)
 
     }
 }
