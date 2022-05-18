aacc06bedde233f7abd4d9ea5ed046837b717e88
bryanares <bryanares12@gmail.com>
2022-05-18 13:09:29 +0300

    add data to database

--- app/src/main/java/com/brian/astro/HomeFragment.kt
+++ app/src/main/java/com/brian/astro/HomeFragment.kt
@@ -1,15 +1,19 @@
 package com.brian.astro
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.view.isVisible
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.brian.astro.data.Space
+import com.brian.astro.data.SpaceViewModel
 import com.brian.astro.databinding.FragmentHomeBinding
 import java.io.IOException
 import java.net.HttpRetryException
@@ -25,19 +29,49 @@ class HomeFragment : Fragment() {
     private val binding get() = _binding!!
     private lateinit var astroAdapter: AstroAdapter
 
+    private lateinit var mSpaceViewModel: SpaceViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        mSpaceViewModel = ViewModelProvider(this).get(SpaceViewModel::class.java)
+
+        astroAdapter = AstroAdapter(mutableListOf(), this)
+        astroAdapter.setOnItemClickListener(object : AstroAdapter.onItemClickListener{
+            override fun onItemClick(position: Int) {
+                insertDataToDatabase()
+            }
+
+        })
         return view
 
     }
     private fun setupRecyclerview() = binding.rvAstro.apply {
-        astroAdapter = AstroAdapter()
+
         adapter = astroAdapter
         layoutManager = LinearLayoutManager(this.context)
+    }
+
+    private fun insertDataToDatabase() {
+        val title = R.id.astroTitle.toString()
+        val description = R.id.astroDescription.toString()
+
+        if(inputCheck(title, description)){
+            //create space object
+            val space = Space(0, title, description)
+            //add data to database
+            mSpaceViewModel.addSpace(space)
+            //notify user of astro save
+            Toast.makeText(requireContext(), "Astro Saved!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun inputCheck(title: String, description: String): Boolean{
+        return  !(TextUtils.isEmpty(title) && TextUtils.isEmpty(description))
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
