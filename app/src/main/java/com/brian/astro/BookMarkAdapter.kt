359818764c39e8adaf8f192ce7fa31795feda82b
bryanares <bryanares12@gmail.com>
2022-05-19 13:18:22 +0300

    adapter

--- app/src/main/java/com/brian/astro/BookMarkAdapter.kt
+++ app/src/main/java/com/brian/astro/BookMarkAdapter.kt
@@ -1,4 +1,49 @@
 package com.brian.astro
 
-class BookMarkAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.brian.astro.data.Space
+import com.brian.astro.databinding.BookmarkItemBinding
+
+
+class BookMarkAdapter : RecyclerView.Adapter<BookMarkAdapter.BookMarkViewHolder>() {
+
+
+    private var spaceList = emptyList<Space>()
+
+    class BookMarkViewHolder(val binding: BookmarkItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookMarkViewHolder {
+        return BookMarkViewHolder(
+            BookmarkItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        ))
+    }
+
+    override fun onBindViewHolder(holder: BookMarkViewHolder, position: Int) {
+        holder.binding.apply {
+            val currentItem = spaceList[position]
+            astroTitle.text = currentItem.title.toString()
+            astroDescription.text = currentItem.description.toString()
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return spaceList.size
+    }
+
+    fun setData(space: List<Space>){
+        this.spaceList = space
+        notifyDataSetChanged()
+
+    }
+
 }
